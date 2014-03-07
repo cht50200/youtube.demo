91c62bc4bdcacab49a0ed61bf5ad04e3b3a8b373
naoto <cht50200@gmail.com>
2014-03-07 15:30:22 +0900

    more changes to index

--- WEB-INF/src/hoge/MyServlet.java
+++ WEB-INF/src/hoge/MyServlet.java
@@ -19,5 +19,6 @@ public class MyServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException{
 		resp.getWriter().println("hello");
+		//aaaa
 	}
 }
